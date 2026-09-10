eb331c42486272f59c2b1482f01a4993c0060de3
Kimi <gennady.evt@gmail.com>
2026-09-10 21:29:45 +0300

    fix: wireguard tunnel lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
     }
 }
 dependencies {
+    implementation("com.wireguard.android:wireguard-android:1.0.20230706")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
